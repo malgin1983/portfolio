345ec9d62b10b6c80b16f4e3a1fd1d9350222323
vadim_m <malghin@yandex.ru>
2026-02-21 18:23:48 +0300

    Поравил конфиг для github

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -7,7 +7,7 @@ import '../../App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
